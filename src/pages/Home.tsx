ffbada11b5aa6e9d6c699e690c73064c652186d7
Kame <kame@ficklewolf.com>
2024-03-18 21:25:59 +0900

    更新

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,17 @@ import './home.scss'
 function Home() {
   return (
     <div className="home">
-      <p>HOME</p>
+      <div className="home-body">
+        <h1 className='home__title'>Profile</h1>
+        <img className='home__img' src="" alt='profile-image' />
+        <h2 className='home__name'>亀山 和希</h2>
+        <p className='home__text'>React.js勉強中</p>
+        <h3 className='home__sub-title'>自己紹介</h3>
+        <p className='home__text'>
+          千葉県出身の27歳です。
+          <br />
+        </p>
+      </div>
     </div>
   )
 }
